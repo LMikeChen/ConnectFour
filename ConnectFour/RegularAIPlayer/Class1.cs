4a65676ca4119b777fd6320476702d051b8ea526
LMikeChen <mike.lchen@gmail.com>
2017-08-09 23:21:22 -0400

    Added Project to implement Regular AI Player

---
+++ ConnectFour/RegularAIPlayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularAIPlayer
+{
+    public class Class1
+    {
+    }
+}
